9f00915c5ea9b6ebc779dd859fcdcaab64491d9f
Esteban Madrigal <40669201+madri308@users.noreply.github.com>
2020-06-12 16:12:47 -0600

    added main
    Se agrego un archivo main y y se consigue el pixel y su color con el mouse

--- imageReader.py
+++ imageReader.py
@@ -1,19 +1,27 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-from PIL import Image
 
 class imageReader:
+    pixels = []
     def __init__(self, url):
         self.url = url
-    def getImage(self):
         img = cv2.imread(self.url)
-        imgplot = plt.imshow(img)
+        self.RGB_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.pixels = np.array(self.RGB_img)
+    def showImage(self):
+        cid = plt.figure().canvas.mpl_connect('button_press_event', self.onclick)
+        plt.imshow(self.pixels)
         plt.show()
-
-        with Image.open('path/to/file.jpg') as img:
-        img.show()
-
-reader1 = imageReader('C:/Users/emema/Documents/TEC/2020/SEM_I/Analisis/New-Flower/f1.jpg')
-reader1.getImage()
+    def onclick(self,event):
+        try:
+            if event.ydata is not None or event.xdata is not None:
+                y = int(event.ydata)
+                x = int(event.xdata)
+                print('color=%s, xPixel=%f, yPixel=%f' %
+                    (self.RGB_img[y,x], x, y))
+                print(self.pixels[2][2])
+            else:
+                raise ValueError("What's up with that?")
+        except ValueError:
+            print("Oops!  Estas afuera de la imagen. Intenta de nuevo...")
---
+++ main.py
@@ -0,0 +1,5 @@
+from imageReader import * 
+
+class main():
+    reader1 = imageReader('C:/Users/emema/Documents/TEC/2020/SEM_I/Analisis/New-Flower/f1.jpg')
+    reader1.showImage()

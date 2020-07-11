bc01d0434b5ef37369e7c478f21d65143c5f6774
Zhong <liuguo35@hotmail.com>
2020-07-11 14:17:34 -0600

    Mejora en Cruce y Mutacion

--- geneticOperator.py
+++ geneticOperator.py
@@ -5,31 +5,27 @@ class GeneticOperator(IConstant):
     def __init__(self):
         self.size=0
 
-    def convertBin(self,numero):
-        numero = bin(numero)[2:]
-        completado = ''
-        for x in range(self.cantidadBits-len(numero)):
-            completado += '0'
-        return completado + numero
+    def modifyBit(self, gen,  posicion,  newBit):
+        return (gen & ~(1<<(self.cantidadBits-posicion))) | (newBit<<(self.cantidadBits-posicion))
 
     def cross(self,gen1,gen2):
         punto = random.randint(5,self.cantidadBits-5) #Se elige un punto para hacer el intercambio
-        newIndividuo = ''
-        gen1 = self.convertBin(gen1)
-        gen2 = self.convertBin(gen2)
-        newIndividuo = gen1[:punto] + gen2[punto:] #Se mezcla el material genetico de los padres en cada nuevo individuo
-        return int(newIndividuo,2)
+
+        gen1 = gen1 >> (self.cantidadBits-punto)
+        gen1 = gen1 << (self.cantidadBits-punto)
+
+        gen2 = gen2 & (2**(self.cantidadBits-punto) - 1)
+
+        return gen1 ^ gen2
 
     def mutate(self,newGen):
         if (random.random() <= 0.075):
-            punto = random.randint(0,self.cantidadBits-1)
-            #print(punto)
-            gen = self.convertBin(newGen)
-            if (gen[punto] == '0'):
-                gen = gen[:punto] + '1' + gen[punto+1:] 
-            else:
-                gen = gen[:punto] + '0' + gen[punto+1:]
-            return int(gen,2)
+            punto = random.randint(1,self.cantidadBits)
+            bitNumber = newGen & (1 << (self.cantidadBits-punto))
+            bitNumber = bitNumber >> (self.cantidadBits-punto)
+            if bitNumber == 0:
+                return modifyBit(newGen,punto,1)
+            return modifyBit(newGen,punto,0)
         else:
             return newGen
     
